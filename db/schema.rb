df42a63c32d6146b0ef41886045b37b71d0190a1
Humberto <hdkaratechop@gmail.com>
2022-12-30 17:13:39 -0800

    add reference to user model

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_30_092115) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
